afa36e789f348bc338f33bd429100c23a079992a
Bruce Berkowicz <bruceberk@me.com>
2021-12-01 17:16:58 -0500

    starting day 1 using pytest

---
+++ day01.py
@@ -0,0 +1,6 @@
+# day 01 - Count values that are increasing
+
+def count_increase(fname: str) -> int:
+    """Count lines in a text file that are higher than the previous"""
+
+    return 0
---
+++ test_day01a.py
@@ -0,0 +1,6 @@
+import pytest
+import day01
+
+
+def test_part1():
+    assert 7 == day01.count_increase('test_day01.txt')
